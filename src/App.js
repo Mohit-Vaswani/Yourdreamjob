bfc9a026404db056ccf455595f9c79caf0c1429a
Mohit Vaswani <mohitvaswani010@gmail.com>
2022-11-09 13:20:36 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import CompanyCard from "./Components/CompanyCard";
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-215720867-1', [options]);
+
+ReactGA.pageview(window.location.pathname);
 
 function App() {
   return (
